1e2ed8589a22c15e4ea3c015573173f13ec16d7a
Daniel Daszkiewicz <dd482@njit.edu>
2023-03-20 18:33:55 -0400

    PcfLocal not working in bash script

--- wpi-copy.sh
+++ wpi-copy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # This script is a template for the WPI loop for a project with -Ainfer=ajava
 # added to its build file. Fill in the variables at the beginning of the
@@ -8,7 +8,7 @@
 # Where should the output be placed at the end? This directory is also
 # used to store intermediate WPI results. The directory does not need to
 # exist. If it does exist when this script starts, it will be deleted.
-WPITEMPDIR=/tmp/WPITEMPDIR
+WPITEMPDIR=/tmp/WPITEMP-require-javadoc
 
 # Where is WPI's output placed by the Checker Framework? This is some
 # directory ending in build/whole-program-inference. For most projects,
@@ -17,9 +17,10 @@ WPITEMPDIR=/tmp/WPITEMPDIR
 WPIOUTDIR=~/.gradle/workers/build/whole-program-inference
 #WPIOUTDIR=/home/dan/Documents/research_kellog/require-javadoc/whole-program-inference/
 # The compile and clean commands for the project's build system.
-BUILD_CMD="./gradlew compileJava"
+BUILD_CMD="./gradlew compileJava -PcfLocal"
 CLEAN_CMD="./gradlew clean"
-
+CHECKERFRAMEWORK=/Users/daniel/Documents/njit/WPI-Research/checker-framework/checker-framework-dd482IT
+export CHECKERFRAMEWORK
 # Whether to run in debug mode.
 DEBUG=1
 
@@ -29,6 +30,8 @@ rm -rf ${WPITEMPDIR}
 mkdir ${WPITEMPDIR}
 
 while : ; do
+    CHECKERFRAMEWORK=/Users/daniel/Documents/njit/WPI-Research/checker-framework/checker-framework-dd482IT
+    export CHECKERFRAMEWORK
     if [[ ${DEBUG} == 1 ]]; then
 	echo "entering a new iteration"
     fi
